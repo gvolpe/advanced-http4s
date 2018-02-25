40981c9407f9e093edc052232cabf40c06314115
Gabriel Volpe <volpegabriel@gmail.com>
2018-02-25 17:43:48 +0100

    Using http client bracketed stream wherever possible.

--- src/main/scala/com/github/gvolpe/http4s/client/MultipartClient.scala
+++ src/main/scala/com/github/gvolpe/http4s/client/MultipartClient.scala
@@ -1,21 +1,22 @@
 package com.github.gvolpe.http4s.client
 
 import cats.effect.Effect
-import cats.syntax.flatMap._
 import cats.syntax.functor._
 import com.github.gvolpe.http4s.StreamUtils
 import fs2.StreamApp.ExitCode
 import fs2.{Scheduler, Stream, StreamApp}
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
+import org.http4s.Method._
 import org.http4s.client.blaze.Http1Client
+import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.headers.`Content-Type`
 import org.http4s.multipart.{Multipart, Part}
-import org.http4s.{MediaType, Method, Request, Uri}
+import org.http4s.{MediaType, Uri}
 
 object MultipartClient extends MultipartHttpClient[Task]
 
-class MultipartHttpClient[F[_]](implicit F: Effect[F], S: StreamUtils[F]) extends StreamApp {
+class MultipartHttpClient[F[_]](implicit F: Effect[F], S: StreamUtils[F]) extends StreamApp with Http4sClientDsl[F] {
 
   private val rick = getClass.getResource("/rick.jpg")
 
@@ -27,19 +28,17 @@ class MultipartHttpClient[F[_]](implicit F: Effect[F], S: StreamUtils[F]) extend
   )
 
   private val request =
-    Request[F](method = Method.POST, uri = Uri.uri("http://localhost:8080/v1/multipart"))
-      .withBody(multipart)
+    POST(Uri.uri("http://localhost:8080/v1/multipart"), multipart)
       .map(_.replaceAllHeaders(multipart.headers))
 
   override def stream(args: List[String], requestShutdown: F[Unit]): Stream[F, ExitCode] = {
     Scheduler(corePoolSize = 2).flatMap { implicit scheduler =>
-      Stream.eval(
-        for {
-          client  <- Http1Client[F]()
-          req     <- request
-          _       <- client.expect[String](req).map(println)
-        } yield ()
-      )
+      for {
+        client <- Http1Client.stream[F]()
+        req    <- Stream.eval(request)
+        value  <- Stream.eval(client.expect[String](req))
+        _      <- S.evalF(println(value))
+      } yield ()
     }.drain
   }
 
