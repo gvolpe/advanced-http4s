012946e6a7f6a74b00afc92ecab81b971f2b6f54
jules Ivanic <jules.ivanic@gmail.com>
2018-12-28 17:54:25 +0100

    [WIP] Update dependencies

--- src/main/scala/com/github/gvolpe/fs2/PubSub.scala
+++ src/main/scala/com/github/gvolpe/fs2/PubSub.scala
@@ -22,29 +22,35 @@ final case class Event(value: String) extends AnyVal
 object PubSubApp extends IOApp {
   import cats.syntax.all._
 
-  def stream[F[_]: ConcurrentEffect: Timer](implicit F: ConcurrentEffect[F], T: Timer[F]): fs2.Stream[F, Unit] =
-      for {
-        topic     <- Stream.eval(Topic[F, Event](Event("")))
-        signal    <- Stream.eval(SignallingRef[F, Boolean](false))
-        service   = new EventService[F](topic, signal)
-        _  <- Stream(T.sleep(15.seconds) *> F.delay(Stream.eval(signal.set(true))),
-                      service.startPublisher concurrently service.startSubscribers
-                    ).parJoin(2)
-      } yield ()
+  def stream[F[_]](implicit F: ConcurrentEffect[F],
+                   T: Timer[F]): fs2.Stream[F, Unit] =
+    for {
+      topic <- Stream.eval(Topic[F, Event](Event("")))
+      signal <- Stream.eval(SignallingRef[F, Boolean](false))
+      service = new EventService[F](topic, signal)
+      emitStopSignal = Stream.sleep(15.seconds) *> Stream.eval(signal.set(true))
+      starts = service.startPublisher concurrently service.startSubscribers
+      _ <- Stream(emitStopSignal, starts).parJoin[F, Unit](2)
+    } yield ()
 
   override def run(args: List[String]): IO[ExitCode] =
     stream[IO].compile.drain.as(ExitCode.Success)
 
 }
 
-class EventService[F[_]: Timer](eventsTopic: Topic[F, Event], interrupter: Signal[F, Boolean])(implicit F: Concurrent[F]) {
+class EventService[F[_]: Timer](
+    eventsTopic: Topic[F, Event],
+    interrupter: Signal[F, Boolean])(implicit F: Concurrent[F]) {
 
   // Publishing events every one second until signaling interruption
   def startPublisher: Stream[F, Unit] =
-    Stream.awakeEvery[F](1.second).flatMap { _ =>
-      val event = Event(System.currentTimeMillis().toString)
-      Stream.eval(eventsTopic.publish1(event))
-    }.interruptWhen(interrupter)
+    Stream
+      .awakeEvery[F](1.second)
+      .flatMap { _ =>
+        val event = Event(System.currentTimeMillis().toString)
+        Stream.eval(eventsTopic.publish1(event))
+      }
+      .interruptWhen(interrupter)
 
   // Creating 3 subscribers in a different period of time and join them to run concurrently
   def startSubscribers: Stream[F, Unit] = {
@@ -53,7 +59,8 @@ class EventService[F[_]: Timer](eventsTopic: Topic[F, Event], interrupter: Signa
     val s3: Stream[F, Event] = eventsTopic.subscribe(10).delayBy(10.seconds)
 
     def sink(subscriberNumber: Int): Sink[F, Event] =
-      _.evalMap(e => F.delay(println(s"Subscriber #$subscriberNumber processing event: $e")))
+      _.evalMap(e =>
+        F.delay(println(s"Subscriber #$subscriberNumber processing event: $e")))
 
     Stream(s1 to sink(1), s2 to sink(2), s3 to sink(3)).parJoin(3)
   }
