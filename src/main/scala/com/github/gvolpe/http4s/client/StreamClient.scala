fb4b936674f15ab80c4bf0107cf16b407135435c
jules Ivanic <jules.ivanic@gmail.com>
2018-12-28 16:53:34 +0100

    [WIP] Update dependencies

--- src/main/scala/com/github/gvolpe/http4s/client/StreamClient.scala
+++ src/main/scala/com/github/gvolpe/http4s/client/StreamClient.scala
@@ -1,32 +1,30 @@
 package com.github.gvolpe.http4s.client
 
-import cats.effect.{ConcurrentEffect, ExitCode, IO}
-import com.github.gvolpe.fs2.PubSubApp.stream
+import cats.effect.{ConcurrentEffect, ExitCode, IO, IOApp}
 import com.github.gvolpe.http4s.StreamUtils
 import fs2.Stream
 import io.circe.Json
 import jawn.RawFacade
-import monix.eval.TaskApp
-import monix.execution.Scheduler.Implicits.global
+import monix.execution.Scheduler
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.{Request, Uri}
 
-object StreamClient extends TaskApp {
+object StreamClient extends IOApp {
+  import cats.implicits._
 
   implicit final val jsonFacade: RawFacade[Json] =
     io.circe.jawn.CirceSupportParser.facade
 
   def stream[F[_]: ConcurrentEffect](implicit S: StreamUtils[F]): Stream[F, Unit] =
-    BlazeClientBuilder[F](global).stream.flatMap { client =>
+    BlazeClientBuilder[F](Scheduler.global).stream.flatMap { client =>
       val request = Request[F](uri = Uri.uri("http://localhost:8080/v1/dirs?depth=3"))
       for {
-        response <- client.streaming(request)(
-          _.body.chunks.through(fs2.text.utf8DecodeC))
-        _ <- S.putStr(response)
+        response <- client.stream(request).flatMap(_.body.chunks.through(fs2.text.utf8DecodeC))
+        _        <- S.putStr(response)
       } yield ()
     }
 
   override def run(args: List[String]): IO[ExitCode] =
-    stream.compile.drain.as(ExitCode.Success)
+    stream[IO].compile.drain.as(ExitCode.Success)
 
 }
--- src/main/scala/com/github/gvolpe/http4s/server/Server.scala
+++ src/main/scala/com/github/gvolpe/http4s/server/Server.scala
@@ -3,17 +3,17 @@ package com.github.gvolpe.http4s.server
 import cats.data.Kleisli
 import cats.effect._
 import fs2.Stream
-import monix.eval.{Task, TaskApp}
-import monix.execution.Scheduler.Implicits.global
+import monix.execution.Scheduler
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.{Request, Response}
 
-object Server extends TaskApp {
-  import org.http4s.implicits._
+object Server extends IOApp {
+  import cats.implicits._
 
-  private def app[F[_]: Sync](ctx: Module[F]): Kleisli[F, Request[F], Response[F]] =
+  private def app[F[_]: Sync](
+      ctx: Module[F]): Kleisli[F, Request[F], Response[F]] =
     Router(
       "/" -> ctx.httpServices,
       s"/${endpoints.ApiVersion}" -> ctx.fileHttpEndpoint,
@@ -23,7 +23,7 @@ object Server extends TaskApp {
 
   def stream[F[_]: ConcurrentEffect: Timer]: Stream[F, Unit] =
     for {
-      client <- BlazeClientBuilder[F](global).stream
+      client <- BlazeClientBuilder[F](Scheduler.global).stream
       ctx <- Stream(new Module[F](client))
       exitCode <- BlazeServerBuilder[F]
         .bindHttp(8080, "0.0.0.0")
@@ -31,5 +31,6 @@ object Server extends TaskApp {
         .serve
     } yield exitCode
 
-  override def run(args: List[String]): Task[ExitCode] = stream[IO].compile.drain.as(ExitCode.Success)
+  override def run(args: List[String]): IO[ExitCode] =
+    stream[IO].compile.drain.as(ExitCode.Success)
 }
