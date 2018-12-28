9ec99e1bb22ccb062c1906b9da471e09df29318f
jules Ivanic <jules.ivanic@gmail.com>
2018-12-28 17:57:19 +0100

    [WIP] Update dependencies

--- src/main/scala/com/github/gvolpe/http4s/server/endpoints/auth/BasicAuthHttpEndpoint.scala
+++ src/main/scala/com/github/gvolpe/http4s/server/endpoints/auth/BasicAuthHttpEndpoint.scala
@@ -1,7 +1,6 @@
 package com.github.gvolpe.http4s.server.endpoints.auth
 
 import cats.effect.Sync
-import com.github.gvolpe.http4s.server.endpoints.ApiVersion
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.AuthMiddleware
