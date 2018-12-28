8306ae19751f89b477c5374fc6320206755a0721
jules Ivanic <jules.ivanic@gmail.com>
2018-12-28 17:18:45 +0100

    [WIP] Update dependencies

--- src/main/scala/com/github/gvolpe/http4s/server/endpoints/TimeoutHttpEndpoint.scala
+++ src/main/scala/com/github/gvolpe/http4s/server/endpoints/TimeoutHttpEndpoint.scala
@@ -2,21 +2,22 @@ package com.github.gvolpe.http4s.server.endpoints
 
 import java.util.concurrent.TimeUnit
 
-import cats.effect.Async
-import fs2.Scheduler
+import cats.effect.{Concurrent, Timer}
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Random
 
-class TimeoutHttpEndpoint[F[_]](implicit F: Async[F], S: Scheduler) extends Http4sDsl[F] {
+class TimeoutHttpEndpoint[F[_]](implicit F: Concurrent[F], T: Timer[F]) extends Http4sDsl[F] {
 
-  val service: HttpService[F] = HttpService {
+  import cats.implicits._
+
+  val service: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / ApiVersion / "timeout" =>
       val randomDuration = FiniteDuration(Random.nextInt(3) * 1000L, TimeUnit.MILLISECONDS)
-      S.effect.delay(Ok("delayed response"), randomDuration)
+      val response = Ok("delayed response")
+      F.race(response, T.sleep(randomDuration) *> GatewayTimeout()).map(_.merge)
   }
 
 }
