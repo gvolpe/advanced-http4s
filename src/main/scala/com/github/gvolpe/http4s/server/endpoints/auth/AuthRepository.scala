15ff26ca31f56d2d5859c9c984a3443d5c5578a1
jules Ivanic <jules.ivanic@gmail.com>
2018-12-28 17:57:52 +0100

    [WIP] Update dependencies

--- src/main/scala/com/github/gvolpe/http4s/server/endpoints/auth/AuthRepository.scala
+++ src/main/scala/com/github/gvolpe/http4s/server/endpoints/auth/AuthRepository.scala
@@ -9,13 +9,14 @@ trait AuthRepository[F[_], A] {
 }
 
 object AuthRepository {
+  import cats.implicits._
 
   implicit def authUserRepo[F[_]](implicit F: Sync[F]): AuthRepository[F, BasicCredentials] =
     new AuthRepository[F, BasicCredentials] {
       private val storage = scala.collection.mutable.Set[BasicCredentials](
         BasicCredentials("gvolpe", "123456")
       )
-      override def persist(entity: BasicCredentials): F[Unit] = F.delay(storage.add(entity))
+      override def persist(entity: BasicCredentials): F[Unit] = F.delay(storage.add(entity)).void
       override def find(entity: BasicCredentials): F[Option[BasicCredentials]] = F.delay(storage.find(_ == entity))
     }
 
