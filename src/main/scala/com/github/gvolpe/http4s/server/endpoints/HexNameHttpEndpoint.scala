ad73239cb5c3560b9461445ad7e38b8cc92a835a
jules Ivanic <jules.ivanic@gmail.com>
2018-12-28 17:56:51 +0100

    [WIP] Update dependencies

--- src/main/scala/com/github/gvolpe/http4s/server/endpoints/HexNameHttpEndpoint.scala
+++ src/main/scala/com/github/gvolpe/http4s/server/endpoints/HexNameHttpEndpoint.scala
@@ -1,14 +1,14 @@
 package com.github.gvolpe.http4s.server.endpoints
 
-import cats.Monad
-import org.http4s._
+import cats.effect.Sync
+import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
-class HexNameHttpEndpoint[F[_]: Monad] extends Http4sDsl[F] {
+class HexNameHttpEndpoint[F[_]: Sync] extends Http4sDsl[F] {
 
   object NameQueryParamMatcher extends QueryParamDecoderMatcher[String]("name")
 
-  val service: HttpService[F] = HttpService {
+  val service: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / ApiVersion / "hex" :? NameQueryParamMatcher(name) =>
       Ok(name.getBytes("UTF-8").map("%02x".format(_)).mkString)
   }
